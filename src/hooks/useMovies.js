1e08093bf0145835a0a6cbd97690bdfb3b9febf2
santiago-federici <santiagofederici88@gmail.com>
2023-08-07 11:11:32 -0300

    useRef for not searching the same movie twice

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { findMovies } from '../services/movies.js'
 
 export function useMovies({search}) {
   const [movies, setMovies] = useState([])
+  const previousSearch = useRef(search)
   
-
   const getMovies = async () => {
+    if(search == previousSearch.current) return
+    previousSearch.current = search
     const newMovies = await findMovies({search})
     setMovies(newMovies)
   }
